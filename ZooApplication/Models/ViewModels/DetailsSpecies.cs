859b7e700f791a3508564197719469ccaea0f421
Somebody <christinebitts@gmail.com>
2021-06-03 16:01:53 -0400

    update fix

--- ZooApplication/Models/ViewModels/DetailsSpecies.cs
+++ ZooApplication/Models/ViewModels/DetailsSpecies.cs
@@ -7,7 +7,10 @@ namespace ZooApplication.Models.ViewModels
 {
     public class DetailsSpecies
     {
+        //the species itself that we want to display
         public SpeciesDto SelectedSpecies { get; set; }
+
+        //all of the related animals to that particular species
         public IEnumerable<AnimalDto> RelatedAnimals { get; set; }
     }
 }
